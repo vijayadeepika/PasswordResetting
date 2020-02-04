e17f5875a673eb73618c020ae34c746f453dda1a
Vijayadeepika Madanu <vijayadeepika.m@ojas-it.com>
2020-02-04 16:11:37 +0530

    second_commit

---
+++ practice_ssrp/Controllers/RegisterController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace practice_ssrp.Controllers
+{
+    public class RegisterController : Controller
+    {
+        // GET: Register
+        public ActionResult Index()
+        {
+            return View();
+        }
+        //[HttpPost]
+        public ActionResult index()
+        {
+            return View();
+        }
+        public ActionResult contact()
+        {
+            return View();
+        }
+    }
+}
